2a97d0dd0cc734d1c93046fbc18fc3bb1e3adb8a
akyla <akylaisyi@yahoo.com>
2023-07-30 07:14:02 -0500

    cukes runner updated

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -6,11 +6,12 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty",
-                "html:target//cucumber-reports.html",
-                "rerun:target/rerunFailedTC.txt",
-                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
-        },
+        plugin = {
+                "json:target/cucumber.json",
+                "html:target/cucumber/report.html",
+                "junit:target/junit/junit-report.xml",
+                "rerun:target/rerun.txt",},
+
        features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
